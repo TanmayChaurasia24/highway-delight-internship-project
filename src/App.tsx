596a846d08c4a473fb277d379e07134c01f9cedc
tanmay kumar chaurasia <130184741+TanmayChaurasia24@users.noreply.github.com>
2024-09-14 23:35:01 +0530

    project setup done

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="text-center">
+      <h1 className="">Hello, Tailwind with React + TypeScript!</h1>
+    </div>
+  );
+};
+
+export default App;
